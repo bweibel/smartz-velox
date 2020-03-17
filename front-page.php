ec2bfe7a068051b2e454c828dc6b90a576992dda
Ben Weibel <weibel@gmail.com>
2020-03-17 15:37:39 -0700

    Added stubs for front page section

--- front-page.php
+++ front-page.php
@@ -20,6 +20,19 @@ if ( is_home() ) {
 
 ?>
 	<main id="primary" class="site-main">
+
+		<!-- #calltoaction -->
+		<section class="call-to-action">
+				<h2>Work better.<br>Play more.</h2>
+				<h3>I.T. support you can trust</h3>
+				<p>We help you work better by providing integrated systems management and strategic forecasting to keep your time at work focused on, well, work! When you leave for the day, you can leave knowing your environment and data are secure and ready for your return. Who knew work-life-balance could be so sustainable?</p>
+				<p>You can play more by becoming our strategic partner and letting us do the heavy lifting. Need a well-designed network? Smart applications? Cloud migration assistance? We’ll get those designs in place and provide a road-map of how to achieve those goals so you can enjoy life beyond planning.</p>
+			<figure>
+				<img src="wp-content/themes/velox-dev/assets/images/Velox-Caricatures-Jordan@2x.png" alt="">
+			</figure>
+		</section>
+		<!-- #calltoaction -->
+		
 		<?php
 
 		while ( have_posts() ) {
@@ -28,8 +41,47 @@ if ( is_home() ) {
 			get_template_part( 'template-parts/content/entry', get_post_type() );
 		}
 
-		get_template_part( 'template-parts/content/pagination' );
+		// get_template_part( 'template-parts/content/pagination' );
 		?>
+
+		<!-- #services -->
+		<section class="services">
+				<h2>Your IT Environment, Synchronized</h2>
+				<p>Velox Systems has a wide variety of tools to create a secure business environment, tailored to your specific needs. These tools combined with our experience will provide a cohesive working experience for you and your team. </p>
+				<div class="container">
+					<div class="service"></div>
+					<div class="service"></div>
+					<div class="service"></div>
+					<div class="service"></div>
+					<div class="service"></div>
+					<div class="service"></div>
+				</div>
+		</section>
+		<!-- #services -->
+		
+		<!-- #video -->
+		<section>
+				<h2>The Velox Advantage</h2>
+				<p>Competitive markets require every advantage available to you with managed IT services, your business can outpace your competitors. Take advantage of the IT services that Velox Systems offers to put you ahead of the competition.</p>
+				<iframe width="560" height="315" src="https://www.youtube.com/embed/aIUQ7DsOCg0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				<a href="#" class="button">Meet the Team</a>
+		</section>
+		<!-- #video -->
+		<!-- #testimonials -->
+		<section>
+				<h2>What Our Customers Are Saying</h2>
+
+		</section>
+		<!-- #testimonials -->
+		
+		<!-- #testimonials -->
+		<section class="blog">
+				<h2>Latest News</h2>
+
+		</section>
+    <!-- #testimonials -->
+
+
 	</main><!-- #primary -->
 <?php
 get_footer();
