79075d9a9291f9e7a323fa161a875b30358f1f01
Ben Weibel <weibel@gmail.com>
2020-03-17 14:46:58 -0700

    Added standard page and team page templates

---
+++ team-page.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Template Name: Team Template
+ *
+ * Page Template for Meet the Team page
+ * *
+ * @link https://developer.wordpress.org/themes/template-files-section/page-template-files/#creating-custom-page-templates-for-global-use
+ *
+ * @package wp_rig
+ */
+
+namespace WP_Rig\WP_Rig;
+
+get_header();
+
+wp_rig()->print_styles( 'wp-rig-content' );
+
+?>
+	<main id="primary" class="site-main">
+		<?php
+
+		while ( have_posts() ) {
+			the_post();
+
+			get_template_part( 'template-parts/content/entry', 'page' );
+		}
+		?>
+	</main><!-- #primary -->
+<?php
+get_sidebar();
+get_footer();
