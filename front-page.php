4af5d2dd8f2001e72d7ade6dd916f8c7fbf8ee94
Ben Weibel <weibel@gmail.com>
2020-03-17 20:59:29 -0700

    Call to Action element styling

--- front-page.php
+++ front-page.php
@@ -23,13 +23,17 @@ if ( is_home() ) {
 
 		<!-- #call to action -->
 		<section class="call-to-action fancy-background">
-				<h2>Work better.<br>Play more.</h2>
-				<h3>I.T. support you can trust</h3>
-				<p>We help you work better by providing integrated systems management and strategic forecasting to keep your time at work focused on, well, work! When you leave for the day, you can leave knowing your environment and data are secure and ready for your return. Who knew work-life-balance could be so sustainable?</p>
-				<p>You can play more by becoming our strategic partner and letting us do the heavy lifting. Need a well-designed network? Smart applications? Cloud migration assistance? We’ll get those designs in place and provide a road-map of how to achieve those goals so you can enjoy life beyond planning.</p>
-			<figure>
-				<img src="wp-content/themes/velox-dev/assets/images/Velox-Caricatures-Jordan@2x.png" alt="">
-			</figure>
+			<div class="wrap wrap-large">
+				<section class="pitch">
+					<h2 class="title">Work better.<br>Play more.</h2>
+					<h3 class="subtitle">I.T. support you can trust</h3>
+					<p>We help you work better by providing integrated systems management and strategic forecasting to keep your time at work focused on, well, work! When you leave for the day, you can leave knowing your environment and data are secure and ready for your return. Who knew work-life-balance could be so sustainable?</p>
+					<p>You can play more by becoming our strategic partner and letting us do the heavy lifting. Need a well-designed network? Smart applications? Cloud migration assistance? We’ll get those designs in place and provide a road-map of how to achieve those goals so you can enjoy life beyond planning.</p>
+				</section>
+				<figure>
+					<img src="wp-content/themes/velox-dev/assets/images/Velox-Caricatures-Jordan@2x.png" alt="">
+				</figure>
+			</div>
 		</section>
 		<!-- #call to action -->
 		
