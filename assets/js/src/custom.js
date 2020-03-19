42a195b0ed2c8164c687bad9fd578d5c0d224148
Ben Weibel <weibel@gmail.com>
2020-03-18 23:36:50 -0700

    Add button styling

---
+++ assets/js/src/custom.js
@@ -0,0 +1,14 @@
+
+
+if ( 'loading' === document.readyState ) {
+	// The DOM has not yet been loaded.
+	document.addEventListener( 'DOMContentLoaded', init );
+} else {
+	// The DOM has already been loaded.
+	init();
+}
+
+// Initiate the menus when the DOM loads.
+function init() {
+  console.log("test");
+}
--- front-page.php
+++ front-page.php
@@ -29,6 +29,8 @@ if ( is_home() ) {
 					<h3 class="subtitle">I.T. support you can trust</h3>
 					<p>We help you work better by providing integrated systems management and strategic forecasting to keep your time at work focused on, well, work! When you leave for the day, you can leave knowing your environment and data are secure and ready for your return. Who knew work-life-balance could be so sustainable?</p>
 					<p>You can play more by becoming our strategic partner and letting us do the heavy lifting. Need a well-designed network? Smart applications? Cloud migration assistance? We’ll get those designs in place and provide a road-map of how to achieve those goals so you can enjoy life beyond planning.</p>
+					<p>Not sure where to go?</p>
+					<a href="#" class="button button--big">Let's Figure it Out Together</a>
 				</section>
 				<figure>
 					<img src="wp-content/themes/velox-dev/assets/images/Velox-Caricatures-Jordan@2x.png" alt="">
