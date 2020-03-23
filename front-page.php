092328b2cde1558b0ab498a5f6a5e2b0893ce7e8
Ben Weibel <weibel@gmail.com>
2020-03-22 22:27:44 -0700

    Seperate call to action template part

--- front-page.php
+++ front-page.php
@@ -9,8 +9,6 @@
 
 namespace WP_Rig\WP_Rig;
 
-$theme = get_template_directory_uri();
-
 get_header();
 
 // Use grid layout if blog index is displayed.
@@ -24,21 +22,7 @@ if ( is_home() ) {
 	<main id="primary" class="site-main">
 
 		<!-- #call to action -->
-		<section class="call-to-action fancy-background sloped">
-			<div class="wrap wrap-large">
-				<section class="pitch">
-					<h1 class="title">Work better.<br>Play more.</h1>
-					<h3 class="subtitle">I.T. support you can trust</h3>
-					<p>We help you work better by providing integrated systems management and strategic forecasting to keep your time at work focused on, well, work! When you leave for the day, you can leave knowing your environment and data are secure and ready for your return. Who knew work-life-balance could be so sustainable?</p>
-					<p>You can play more by becoming our strategic partner and letting us do the heavy lifting. Need a well-designed network? Smart applications? Cloud migration assistance? We’ll get those designs in place and provide a road-map of how to achieve those goals so you can enjoy life beyond planning.</p>
-					<p>Not sure where to go?</p>
-				</section>
-				<figure>
-					<img src=<?php printf($theme . "/assets/images/Velox-Caricatures-Jordan@2x.png")?> alt="Caricature" role="presentation">
-				</figure>
-				<a href="#" class="button button--big">Let's Figure it Out Together</a>
-			</div>
-		</section>
+		<?php get_template_part( 'template-parts/content/call_to_action' ); ?>
 		<!-- #call to action -->
 		
 		<!-- #standard page content -->
@@ -118,15 +102,15 @@ if ( is_home() ) {
 		</section>
 		<!-- #testimonials -->
 		
-		<!-- #Blog -->
-		<section class="blog fancy-background">
+		<!-- #Featured Post -->
+		<section class="featured-post fancy-background">
 			<div class="wrap">
 				<h1>Latest News</h1>
 				[Blog Element]
 					</div>
 				
 		</section>
-    <!-- #Blog -->
+    <!-- #Featured Post -->
 
 
 	</main><!-- #primary -->
---
+++ template-parts/content/call_to_action.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Template part for displaying the call to action
+ *
+ * @package wp_rig
+ */
+
+namespace WP_Rig\WP_Rig;
+
+$theme = get_template_directory_uri();
+?>
+<!-- #call to action -->
+<section class="call-to-action fancy-background sloped">
+  <div class="wrap wrap-large">
+    <section class="pitch">
+      <h1 class="title">Work better.<br>Play more.</h1>
+      <h3 class="subtitle">I.T. support you can trust</h3>
+      <p>We help you work better by providing integrated systems management and strategic forecasting to keep your time at work focused on, well, work! When you leave for the day, you can leave knowing your environment and data are secure and ready for your return. Who knew work-life-balance could be so sustainable?</p>
+      <p>You can play more by becoming our strategic partner and letting us do the heavy lifting. Need a well-designed network? Smart applications? Cloud migration assistance? We’ll get those designs in place and provide a road-map of how to achieve those goals so you can enjoy life beyond planning.</p>
+      <p>Not sure where to go?</p>
+    </section>
+    <figure>
+      <img src=<?php printf($theme . "/assets/images/Velox-Caricatures-Jordan@2x.png")?> alt="Caricature" role="presentation">
+    </figure>
+    <a href="#" class="button button--big">Let's Figure it Out Together</a>
+  </div>
+</section>
+<!-- #call to action -->
