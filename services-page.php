50d747d5dbb19a66680dd904cb27deeaa18963b1
Ben Weibel <weibel@gmail.com>
2020-03-23 08:20:21 -0700

    Add services page

---
+++ services-page.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Template Name: Services Template
+ *
+ * Page Template for Meet the Team page
+ * *
+ * @link https://developer.wordpress.org/themes/template-files-section/page-template-files/#creating-custom-page-templates-for-global-use
+ *
+ * @package wp_rig
+ */
+
+namespace WP_Rig\WP_Rig;
+
+get_header();
+
+wp_rig()->print_styles( 'wp-rig-content' );
+
+?>
+	<main id="primary" class="site-main">
+		<?php
+
+		while ( have_posts() ) {
+			the_post();
+
+			get_template_part( 'template-parts/content/entry', 'page' );
+			
+			get_template_part('template-parts/content/services');
+		}
+		?>
+	</main><!-- #primary -->
+<?php
+get_footer();
