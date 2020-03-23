b55ffa63c6ab92a9d608f38afb322ade0fba1a46
Ben Weibel <weibel@gmail.com>
2020-03-23 04:32:26 -0700

    Add header Phone number

---
+++ template-parts/header/phone.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Template part for displaying the custom header media
+ *
+ * @package wp_rig
+ */
+
+namespace WP_Rig\WP_Rig;
+?>
+<a href="tel:1-541-639-4397" class="phone">(541) 639-4397</a>
