0d272dca07c5c2a5842949ef6b923ce26428050e
Ben Weibel <weibel@gmail.com>
2020-03-22 22:28:52 -0700

    Add services template part

---
+++ template-parts/content/services.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Template part for displaying the call to action
+ *
+ * @package wp_rig
+ */
+
+namespace WP_Rig\WP_Rig;
+
+$theme = get_template_directory_uri();
+?>
+<section class="services sloped">
+			<div class="wrap">
+				<h1>Your IT Environment, Synchronized</h1>
+				<p>Velox Systems has a wide variety of tools to create a secure business environment, tailored to your specific needs. These tools combined with our experience will provide a cohesive working experience for you and your team. </p>
+			</div>
+				
+				<div class="container wrap-large">
+					<div class="service" data-image-src="wp-content/uploads/2020/03/VeloxBranding-6-720x480.jpg">
+						<img src=<?php printf( $theme . "/assets/images/Velox-Website-Icon-Cloud.svg") ?> alt="Cloud Icon" class="icon-service">
+						<h3 class="title ">IT Cost Engineering</h3>
+						<div class="icon-arrow"></div>
+						<a href="#" class="service-link"></a>
+					</div>
+					<div class="service">
+						<img src=<?php printf( $theme . "/assets/images/Velox-Website-Icon-Cloud.svg") ?> alt="Cloud Icon" class="icon-service">
+						<h3 class="title ">IT Cost Engineering</h3>
+						<div class="icon-arrow"></div>
+						<a href="#" class="service-link"></a>
+					</div>
+					<div class="service">
+						<img src=<?php printf( $theme . "/assets/images/Velox-Website-Icon-Cloud.svg") ?> alt="Cloud Icon" class="icon-service">
+						<h3 class="title ">IT Cost Engineering</h3>
+						<div class="icon-arrow"></div>
+						<a href="#" class="service-link"></a>
+					</div>
+					<div class="service">
+						<img src=<?php printf( $theme . "/assets/images/Velox-Website-Icon-Cloud.svg") ?> alt="Cloud Icon" class="icon-service">
+						<h3 class="title ">IT Cost Engineering</h3>
+						<div class="icon-arrow"></div>
+						<a href="#" class="service-link"></a>
+					</div>
+					<div class="service">
+						<img src=<?php printf( $theme . "/assets/images/Velox-Website-Icon-Cloud.svg") ?> alt="Cloud Icon" class="icon-service">
+						<h3 class="title ">IT Cost Engineering</h3>
+						<div class="icon-arrow"></div>
+						<a href="#" class="service-link"></a>
+					</div>
+					<div class="service">
+						<img src=<?php printf( $theme . "/assets/images/Velox-Website-Icon-Cloud.svg") ?> alt="Cloud Icon" class="icon-service">
+						<h3 class="title ">IT Cost Engineering</h3>
+						<div class="icon-arrow"></div>
+						<a href="#" class="service-link"></a>
+					</div>
+				</div>
+		</section>
