0d626f5f01deef53f10287854f9cfa17fbafb4e9
Ben Weibel <weibel@gmail.com>
2020-03-29 19:19:36 -0700

    Fix chat bubble toggle

--- template-parts/content/chat_bubble.php
+++ template-parts/content/chat_bubble.php
@@ -8,8 +8,50 @@
 namespace WP_Rig\WP_Rig;
 ?>
 
-<div class="chat-cta" >
-  <button class="close-button" onClick="console.log('test')">
+<script>
+
+if ( 'loading' === document.readyState ) {
+	// The DOM has not yet been loaded.
+	document.addEventListener( 'DOMContentLoaded', init );
+} else {
+	// The DOM has already been loaded.
+	init();
+}
+
+
+function init() {
+  const toggleButton = document.querySelector('.close-button');
+  const chatBubble = document.querySelector('.chat-cta'); 
+
+  chatBubble.hidden = getHiddenValue();
+  if(!chatBubble.hidden) chatBubble.classList.remove('hidden');
+
+  toggleButton.addEventListener('click', (e) => {
+    toggleChat(chatBubble);
+  });
+
+}
+
+function getHiddenValue(){
+  if(sessionStorage.getItem("chatHidden")){
+    return JSON.parse(sessionStorage.getItem("chatHidden"));
+  } else {
+    sessionStorage.setItem("chatHidden", false);
+    return false;
+  }
+}
+
+function toggleChat(chatBubble) {
+  chatBubble.hidden = !chatBubble.hidden;
+  sessionStorage.setItem("chatHidden", chatBubble.hidden);
+  chatBubble.classList.toggle('hidden');
+}
+  
+
+</script>
+
+<div class="chat-cta hidden" name="chat-bubble" >
+  <button class="close-button" onClick="">
     x
   </button>
   <div class="bubble">
@@ -17,6 +59,5 @@ namespace WP_Rig\WP_Rig;
     <a href="/contact" class="button">Get Started</a>
   </div>
   <div class="face"></div>
-  
   
 </div>
