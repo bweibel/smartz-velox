b951604da175ff57d9e471c605306bbccbc4b5a2
Ben Weibel <weibel@gmail.com>
2020-04-13 14:51:04 -0700

    Link footer menu icons

--- template-parts/footer/navigation.php
+++ template-parts/footer/navigation.php
@@ -17,19 +17,19 @@ $themeUri = get_template_directory_uri();
 
 <nav id="site-footer-navigation" class="main-navigation nav--toggle-sub nav--toggle-small" aria-label="<?php esc_attr_e( 'Main menu', 'wp-rig' ); ?>">
 
-  <a class="menu-icon" href="">
+  <a class="menu-icon" href="/services">
     <img src=<?php printf($themeUri . "/assets/images/Velox-Website-Icon-Mobile-Services.svg")?> alt="Services">
     Services
   </a>
-  <a class="menu-icon" href="">
+  <a class="menu-icon" href="/industries">
     <img src=<?php printf($themeUri . "/assets/images/Velox-Website-Icon-Mobile-Industries.svg")?> alt="Industries">
     Industries
   </a>
-  <a class="menu-icon" href="">
+  <a class="menu-icon" href="/resources">
     <img src=<?php printf($themeUri . "/assets/images/Velox-Website-Icon-Mobile-Resources.svg")?> alt="Resources">
     Resources
   </a>
-  <a class="menu-icon" href="">
+  <a class="menu-icon" href="/about">
     <img src=<?php printf($themeUri . "/assets/images/Velox-Website-Icon-Mobile-About.svg")?> alt="Menu">
     About
   </a>
