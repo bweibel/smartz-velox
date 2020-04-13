3a4a5c941240ee8f1b63d709934be52fa8cee45b
Ben Weibel <weibel@gmail.com>
2020-04-13 14:51:29 -0700

    Make extra info visible by default

--- template-parts/wizard/extra_info.php
+++ template-parts/wizard/extra_info.php
@@ -8,8 +8,8 @@
 namespace WP_Rig\WP_Rig;
 ?>
 
-<button class='button extra-toggle'>More Info</button>
-<div class="extra-info hidden">
+<!-- <button class='button extra-toggle'>More Info</button> -->
+<div class="extra-info">
   <hr>
   <?php the_field('option_extra_info'); ?>
 </div>
